1e4ababa0026767b0768ebd2bf558bcb69e46254
Badr Almadi <badr.almadhi95@gmail.com>
2023-06-27 19:38:31 -0600

    added models

---
+++ server/models/product.js
@@ -0,0 +1,41 @@
+const { Schema } = require('mongoose');
+
+// product 
+// how we are connecting 
+const productSchema = new Schema(
+    {
+        productName: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        price: {
+            // add decimal
+            type: Decimal128,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: false,
+        },
+        weight: {
+            type: Number,
+            required: false,
+        },
+        feature: {
+            type: Boolean,
+            required: true,
+        }
+    });
+
+module.exports = productSchema;
---
+++ server/models/user.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const ownerSchema = require('./owner')
+
+
+const userSchema = new Schema(
+    {
+
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isOwner: {
+            type: Boolean,
+            required: true,
+        },
+        ownerData: [ownerSchema],
+        zipCode: {
+            type: Number,
+            required: false,
+            max: 5,
+        },
+
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+
+);
+
+
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this, this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+    next();
+});
+
+
+// compare password for login 
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
