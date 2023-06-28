8c80f31b4702958bebf404a022e71f64ca69cd49
JacobWillis <jacobwillis2012@gmail.com>
2023-06-27 21:01:55 -0400

    trying to deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// Import pages here:
+import Home from './pages/home';
 
-function App() {
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
 export default App;
---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+                Hello world!
+        </h1>
+    )
+}
+
+
+export default Home;
