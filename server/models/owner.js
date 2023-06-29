8925c764211c08a66e8fe8c0b20ebbaf6f24d1a8
Badr Almadi <badr.almadhi95@gmail.com>
2023-06-29 16:41:11 -0600

    added the owner.js

--- server/models/owner.js
+++ server/models/owner.js
@@ -1,1 +1,19 @@
-// 
+const { Schema } = require('mongoose');
+
+
+const ownerSchema = new Schema(
+    {
+        ownerName: {
+            type: String,
+            required: true,
+        },
+        ownerStory: {
+            type: String,
+        },
+        ownerImage: {
+            type: String,
+        },
+    }
+);
+
+module.exports = ownerSchema;
