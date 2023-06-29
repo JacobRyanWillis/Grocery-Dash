02cec49142912f46a8c2e8dac52b309a675f5cf2
JacobWillis <jacobwillis2012@gmail.com>
2023-06-29 19:24:11 -0400

    'added about, home, navbar files'

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,22 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
+import Navbar from '../components/navbar'
+import About from '../components/about'
 // import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-                Hello world!
-        </h1>
-    )
-}
+      <div>
+        <Navbar />
+        <div className="h-screen">
+            <h1 className="text-3xl font-bold underline sm:text-4xl md:text-5xl">
+                Choose your farmers market
+            </h1>
+        </div>
+        <About />
+      </div>
+    );
+  }
 
 
 export default Home;
