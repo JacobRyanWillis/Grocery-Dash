4b6eb99c876263f0cba7fd71087dd5f5c6a1c30e
JacobWillis <jacobwillis2012@gmail.com>
2023-07-03 19:11:39 -0400

    'udpated signupintro.js'

--- client/src/pages/signupintro.js
+++ client/src/pages/signupintro.js
@@ -1,38 +1,35 @@
 import React from 'react';
-// import Navbar from '../components/navbar.js';
-// import VendorImage from '../assets/vendor-signup.png';
+import Navbar from '../components/navbar.js';
+import VendorImage from '../assets/VendorImage.png';
+import BuyerImage from '../assets/BuyerImage.png';
 
 const SignupIntro = () => {
   return (
     <div>
-      {/* <Navbar /> */}
-      <div className="h-screen grid grid-cols-2">
-        <div className="bg-gray-200 flex justify-center items-center">
-          <div>
-            <img
-            //   src= {VendorImage}
-              alt="Image 1"
-              className="w-full h-auto"
-            />
-            <div className="mt-4">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                Sign Up as a Vendor
-              </button>
-            </div>
+      <Navbar />
+      <div className="h-screen grid grid-cols-2 font-gilroy">
+        <div className="bg-gray-200 flex justify-center items-center relative brightness-50 hover:brightness-100">
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-center bg-cover"
+            style={{ backgroundImage: `url(${VendorImage})` }}
+          ></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h1 className='text-3xl text-white m-2'> Sell your items </h1>
+            <button className="bg-grass hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
+              Sign Up as a Vendor
+            </button>
           </div>
         </div>
-        <div className="bg-gray-300 flex justify-center items-center">
-          <div>
-            <img
-            //   src={VendorImage}
-              alt="Image 2"
-              className="w-full h-auto"
-            />
-            <div className="mt-4">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                Sign Up as a Buyer
-              </button>
-            </div>
+        <div className="bg-gray-300 flex justify-center items-center relative brightness-50 hover:brightness-100">
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-center bg-cover"
+            style={{ backgroundImage: `url(${BuyerImage})` }}
+          ></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h1 className='text-3xl text-white m-2'>Shop around</h1>
+            <button className="bg-white hover:bg-gray-600 text-eggplant font-bold py-2 px-4 rounded">
+              Sign Up as a Buyer
+            </button>
           </div>
         </div>
       </div>
@@ -41,4 +38,3 @@ const SignupIntro = () => {
 };
 
 export default SignupIntro;
-
