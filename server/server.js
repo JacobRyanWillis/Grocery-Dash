c9968ee3d23ae184b25e146f907123f114f4584b
EverittGill <eg.everittgill@gmail.com>
2023-07-03 21:54:37 -0400

    added some typeDefs and some Resolvers. resolvers will be wrong and need to be updated. added utils folder with auth.js file coppied from mini project

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,6 +12,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +30,7 @@ app.get('/', (req, res) => {
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  server.applyMiddleware({ app });
+  // server.applyMiddleware({ app });
   
   db.once('open', () => {
     app.listen(PORT, () => {
