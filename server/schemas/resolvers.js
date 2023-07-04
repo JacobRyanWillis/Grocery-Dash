e82fc6d63c520d5627b474047d45f1f38b8b46b6
EverittGill <eg.everittgill@gmail.com>
2023-07-03 22:50:23 -0400

    adjusted the typedefs and resolvers. now Apollo Server runs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,57 @@
 
 const resolvers = {
-  Query: {
-    categories: async () => {
-      return await Category.find();
-    },
-    products: async (parent, { category, name }) => {
-      const params = {};
+  // Query: {
+  //   categories: async () => {
+  //     return await Category.find();
+  //   },
+  //   products: async (parent, { category, name }) => {
+  //     const params = {};
 
-      if (category) {
-        params.category = category;
-      }
+  //     if (category) {
+  //       params.category = category;
+  //     }
 
-      if (name) {
-        params.name = {
-          $regex: name
-        };
-      }
+  //     if (name) {
+  //       params.name = {
+  //         $regex: name
+  //       };
+  //     }
 
-      return await Product.find(params).populate('category');
-    },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
-    },
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
+  //     return await Product.find(params).populate('category');
+  //   },
+  //   product: async (parent, { _id }) => {
+  //     return await Product.findById(_id).populate('category');
+  //   },
+  //   user: async (parent, args, context) => {
+  //     if (context.user) {
+  //       const user = await User.findById(context.user._id).populate({
+  //         path: 'orders.products',
+  //         populate: 'category'
+  //       });
 
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+  //       user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
-        return user;
-      }
+  //       return user;
+  //     }
 
-      throw new AuthenticationError('Not logged in');
-    },
-    myList: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
+  //     throw new AuthenticationError('Not logged in');
+  //   },
+  //   myList: async (parent, { _id }, context) => {
+  //     if (context.user) {
+  //       const user = await User.findById(context.user._id).populate({
+  //         path: 'orders.products',
+  //         populate: 'category'
+  //       });
 
-        return user.orders.id(_id);
-      }
+  //       return user.orders.id(_id);
+  //     }
 
-      throw new AuthenticationError('Not logged in');
-    },
+  //     throw new AuthenticationError('Not logged in');
+  //   },
     
-  Mutation: {
-  }}};
+  // Mutation: {
+  // }}};
+};
 
 
 module.exports = resolvers;
