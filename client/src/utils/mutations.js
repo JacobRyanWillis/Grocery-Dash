27ee17b0215ba910b15639caeaf98464420add4b
Brosa <brunoarosa1997@gmail.com>
2023-07-10 20:06:47 -0400

    login and add user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,38 +1,68 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+export const ADD_OWNER = gql`
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+  addOwner(username: $username, email: $email, password: $password) {
+    token
+    owner {
+      _id
     }
   }
+}
+`;
+export const LOGIN_OWNER=gql`
+mutation Mutation($email: String!, $password: String!) {
+  loginOwner(email: $email, password: $password) {
+    token
+    owner {
+      _id
+    }
+  }
+}
 `;
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+export const ADD_BUYER = gql`
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+  addBuyer(username: $username, email: $email, password: $password) {
+    token
+    buyer {
+      _id
+    }
+  }
+}
+`;
+export const LOGIN_BUYER = gql`
+  mutation Mutation($email: String!, $password: String!) {
+    loginBuyer(email: $email, password: $password) {
       token
-      user {
+      buyer {
         _id
-        username
       }
     }
   }
 `;
 
 export const ADD_PRODUCT = gql`
-mutation AddProduct($productName: String!, $description: String!, $category: String!, $price: Float!, $feature: Boolean!) {
-  addProduct(productName: $productName, description: $description, category: $category, price: $price, feature: $feature) {
-    _id
-    myProducts {
+  mutation AddProduct(
+    $productName: String!
+    $description: String!
+    $category: String!
+    $price: Float!
+    $feature: Boolean!
+  ) {
+    addProduct(
+      productName: $productName
+      description: $description
+      category: $category
+      price: $price
+      feature: $feature
+    ) {
       _id
-      productName
-      description
+      myProducts {
+        _id
+        productName
+        description
+      }
     }
   }
-}
 `;
