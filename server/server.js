00b46332e10c5286c697325d8b5470018e610b33
EverittGill <eg.everittgill@gmail.com>
2023-07-11 20:23:43 -0400

    added Stripe functionality

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const chatRoute = require('../server/chat');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -15,45 +16,6 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
-
-
-
-// For stripe+++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-// This is a public sample test API key.
-// Don’t submit any personally identifiable information in requests made with this key.
-// Sign in to see your own test API key embedded in code samples.
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-// const express = require('express');
-// const app = express();
-// app.use(express.static('public'));
-
-const YOUR_DOMAIN = 'http://localhost:4242';
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-  });
-
-  res.redirect(303, session.url);
-});
-
-app.listen(4242, () => console.log('Running on port 4242'));
-
-
-// end for stripe+++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
 
 
 
