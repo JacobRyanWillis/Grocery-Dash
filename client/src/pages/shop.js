72265c55dc6e6047e0391a69a84ac72db422bda6
Brosa <brunoarosa1997@gmail.com>
2023-07-13 03:04:24 -0400

    added states for shop title and owner story

--- client/src/pages/shop.js
+++ client/src/pages/shop.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
-import { GET_PUBLIC_OWNERS, GET_ALL_PRODUCTS} from "../utils/queries";
+import { GET_PUBLIC_OWNERS, GET_ALL_PRODUCTS } from "../utils/queries";
 import Navbar from "../components/navbar";
 import ChatbotIcon from "../components/chatboticon";
 import Select from "react-select";
@@ -15,12 +15,14 @@ const Shop = () => {
   const ownersData = owners?.publicOwners;
 
   const location = useLocation();
-  console.log(location)
-  const defaultProducts =location.state?.owner || productData;
-
+  console.log(location);
+  const defaultProducts = location.state?.owner || productData;
+  const initialTitle = location.state
+    ? `${location.owner.ownerName}`
+    : "Shop All Products";
   const [displayedProducts, setDisplayedProducts] = useState(defaultProducts);
-
-
+  const [pageTitle, setPageTitle] = useState(initialTitle);
+  const [ownerStory, setOwnerStory] = useState("")
 
   const categoryOptions = [
     { value: "all", label: "All Categories" },
@@ -32,29 +34,36 @@ const Shop = () => {
 
   const handleCategories = (selectedOption) => {
     if (selectedOption.value === "all") {
+      setPageTitle("Shop All Products");
       return setDisplayedProducts(productData);
     }
     const filteredProducts = productData?.filter(
       (product) => product.category === selectedOption.value
     );
+    setPageTitle(selectedOption.value);
+    setOwnerStory("")
     setDisplayedProducts(filteredProducts);
   };
 
   const vendors = () => {
-    console.log(ownersData)
-    const filteredVendors = ownersData?.map(
-      (owner) => {return {value: `${owner.ownerName}`, label: `${owner.ownerName}`}} 
-    )
-    console.log(filteredVendors)
-    return filteredVendors
-  }
-  const vendorOptions = vendors()
+    console.log(ownersData);
+    const filteredVendors = ownersData?.map((owner) => {
+      return { value: `${owner.ownerName}`, label: `${owner.ownerName}` };
+    });
+    console.log(filteredVendors);
+    return filteredVendors;
+  };
+  const vendorOptions = vendors();
 
   const handleVendors = (selectedOption) => {
     const vendor = ownersData?.filter(
       (owner) => owner.ownerName === selectedOption.value
     );
-    console.log(vendor)
+    console.log(vendor);
+    setPageTitle(selectedOption.value);
+    // For future development
+    /* setOwnerStory(vendor[0].ownerStory) */
+    setOwnerStory("")
     setDisplayedProducts(vendor[0].myProducts);
   };
 
@@ -63,8 +72,8 @@ const Shop = () => {
       <Navbar />
       <div className="font-gilroy grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
         <div className="flex flex-col justify-center items-center md:items-start col-span-full lg:col-start-2  mx-8 mt-5 md:mt-20">
-          <h1 className="text-5xl font-bold my-2">Mango Boys</h1>
-          <p className="text-xl my-2">Specialties: Fruits and Vegetables</p>
+          <h1 className="text-5xl font-bold my-2">{pageTitle}</h1>
+          <p>{ownerStory}</p>
         </div>
         <div className="col-span-full md:col-span-1 lg:col-span-1 xl:col-span-1 md:text-7xl">
           <div className="p-6">
@@ -95,7 +104,9 @@ const Shop = () => {
             {pLoading ? (
               <p> Products are loading</p>
             ) : (
-              displayedProducts?.map((product) => <SingleProduct product = {product}/>)
+              displayedProducts?.map((product) => (
+                <SingleProduct product={product} />
+              ))
             )}
           </div>
         </div>
