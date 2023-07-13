5246191acf00b9ebbe8461c63234c92805be67d3
JacobWillis <jacobwillis2012@gmail.com>
2023-07-13 18:21:39 -0400

    'reverted changes'

--- client/src/pages/market.js
+++ client/src/pages/market.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { GET_PUBLIC_OWNERS, GET_ALL_PRODUCTS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import Navbar from "../components/navbar";
@@ -9,9 +9,9 @@ import Slider from "react-slick";
 import ChatbotIcon from "../components/chatboticon";
 
 const Market = () => {
-  const { loading, data } = useQuery(GET_PUBLIC_OWNERS);
+  const { data, loading } = useQuery(GET_PUBLIC_OWNERS);
   const userData = data?.publicOwners;
-  console.log(userData);
+  console.log(userData)
 
   const { loading: loading2, data: products } = useQuery(GET_ALL_PRODUCTS);
   const productData = products?.allProducts;
@@ -29,19 +29,48 @@ const Market = () => {
     return feature[randomIndex];
   };
 
-  const renderCarousel = (features) => {
-    const randomFeature = getRandomFeature(features);
+  const renderSection = (category) => {
+    const ownersWithCategory = userData?.filter((owner) =>
+      owner.myProducts.some((product) => product.category === category)
+    );
+    const randomOwner = getRandomOwner(ownersWithCategory);
+
     return (
-      <div className="m-2 flex flex-col justify-center items-center">
-        <img
-          className="h-24 w-24 rounded-full"
-          alt={randomFeature.productName}
-          src={randomFeature.image}
-        ></img>
-        <p className="md:text-lg"> {randomFeature.productName} </p>
+    <Link
+        to="/shop" state={{owner:randomOwner?.myProducts}}
+      >
+      <div className="grid grid-cols-3 shadow-class transition-transform transform hover:scale-105 rounded">
+        <div className="col-span-1 flex items-center">
+          <img
+            className="h-full w-auto rounded"
+            src={randomOwner?.ownerImage}
+            alt={randomOwner?.ownerName}
+          />
+        </div>
+        <div className="col-span-2 flex flex-col justify-center ml-2 pl-2 md:ml-6 md:pl-6">
+          <h3 className="text-md mb-2 text-xl md:text-2xl font-semibold">
+            {randomOwner?.ownerName}
+          </h3>
+          <p className="md:text-lg">{category}</p>
+        </div>
       </div>
+    </Link>
     );
   };
+
+  const renderCarousel = (features) => {
+    const randomFeature = getRandomFeature(features)
+      return (
+        <div className="m-2 flex flex-col justify-center items-center">
+            <img
+              className="h-24 w-24 rounded-full"
+              alt={randomFeature.productName}
+              src={randomFeature.image}
+            ></img>
+            <p className="md:text-lg"> {randomFeature.productName} </p>
+          </div>
+      )      
+  }
 
   const settings = {
     slidesToShow: 5,
@@ -69,33 +98,6 @@ const Market = () => {
         },
       },
     ],
-  };
-
-  const renderSection = (category) => {
-    const ownersWithCategory = userData?.filter((owner) =>
-      owner.myProducts.some((product) => product.category === category)
-    );
-    const randomOwner = getRandomOwner(ownersWithCategory);
-
-    return (
-      <Link to="/shop" state={{ owner: randomOwner?.myProducts }}>
-        <div className="grid grid-cols-3 shadow-class transition-transform transform hover:scale-105 rounded">
-          <div className="col-span-1 flex items-center">
-            <img
-              className="h-full w-auto rounded"
-              src={randomOwner?.ownerImage}
-              alt={randomOwner?.ownerName}
-            />
-          </div>
-          <div className="col-span-2 flex flex-col justify-center ml-2 pl-2 md:ml-6 md:pl-6">
-            <h3 className="text-md mb-2 text-xl md:text-2xl font-semibold">
-              {randomOwner?.ownerName}
-            </h3>
-            <p className="md:text-lg">{category}</p>
-          </div>
-        </div>
-      </Link>
-    );
   };
 
   if (loading) {
@@ -135,10 +137,11 @@ const Market = () => {
         </div>
         <div className="col-span-2">
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 lg:grid-rows-2 gap-4 m-4">
-            {renderSection("Fruits and Vegetables")}
-            {renderSection("Fruits and Vegetables")}
-            {renderSection("Fruits and Vegetables")}
-            {renderSection("Fruits and Vegetables")}
+            {renderSection('Fruits and Vegetables')}
+            {renderSection('Fruits and Vegetables')}
+            {renderSection('Fruits and Vegetables')}
+            {renderSection('Fruits and Vegetables')}
+            
           </div>
         </div>
       </div>
@@ -149,23 +152,23 @@ const Market = () => {
               Meats & Seafood
             </h2>
           </div>
-          {renderSection("Meats and Seafood")}
-          {renderSection("Meats and Seafood")}
+            {renderSection('Meats and Seafood')}
+            {renderSection('Meats and Seafood')}
         </div>
 
         <div className="grid grid-rows-3 gap-4 m-2 p-2">
           <div className="bg-cDairy shadow-class flex justify-start items-start rounded p-4">
             <h2 className="text-3xl md:text-4xl lg:text-5xl">Dairy Products</h2>
           </div>
-          {renderSection("Dairy")}
-          {renderSection("Dairy")}
+            {renderSection('Dairy')}
+            {renderSection('Dairy')}
         </div>
         <div className="grid grid-rows-3 gap-4 m-2 p-2">
           <div className="bg-cBaked shadow-class flex justify-start items-start rounded p-4">
             <h2 className="text-3xl md:text-4xl lg:text-5xl">Baked Goods</h2>
           </div>
-          {renderSection("Baked Goods")}
-          {renderSection("Baked Goods")}
+            {renderSection('Baked Goods')}
+            {renderSection('Baked Goods')}
         </div>
       </div>
       <ChatbotIcon />
